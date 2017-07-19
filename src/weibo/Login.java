41432a3f98e43a7e5bb9f7fa2ca26ef2f1112c0e
Curta7n <Curta7n@live.cn>
2017-07-19 15:08:24 +0800

    添加测试类

--- src/weibo/Login.java
+++ src/weibo/Login.java
@@ -1,5 +1,32 @@
+//package weibo;
+//
+//import java.util.ArrayList;
+//
+//public class Login {
+//	 ArrayList<String> myList = new ArrayList<String>();
+//	 Egg s = new Egg();
+//}
+
 package weibo;
 
 public class Login {
-
+	
+	private String user;
+	private String pwd;
+	
+	public void set_user(String username) {
+		user = username;
+	}
+	
+	public String get_user() {
+		return user;
+	}
+	
+	public void set_pwd(String passwd) {
+		pwd = passwd;
+	}
+	
+	public String get_pwd() {
+		return pwd;
+	}
 }
---
+++ src/weibo/LoginClassTest.java
@@ -0,0 +1,13 @@
+package weibo;
+
+import weibo.Login;
+
+public class LoginClassTest {
+
+	public static void main(String[] args) {
+		Login wb = new Login();
+		wb.set_user("Curtain");
+		wb.set_pwd("lz920723!");
+	}
+
+}
